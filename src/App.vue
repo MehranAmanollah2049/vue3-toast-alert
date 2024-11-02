2b05d3ccee93f78f3614a34e2b4ff94eb842a70e
Mehran <mehranamanollah13@gmail.com>
2024-11-02 16:54:01 +0330

    demo updated

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
 import NotificationContainer from '@/components/Notification/NotificationContainer.vue'
 import toast from '@/composables/Alert.js'
+import { ref } from 'vue';
+
+let isDark = ref(false)
+
+let toggle_theme = () => {
+  isDark.value = !isDark.value
+  document.body.classList.toggle("dark")
+}
+
 </script>
 
 <template>
-  <NotificationContainer />
+  <NotificationContainer :is-dark-mode="isDark" />
 
   <div class="Buttons">
     <div class="btns success" @click="toast('متن پیام  موفقیت  آمیز')">success</div>
     <div class="btns error" @click="toast('متن پیام ارور', 'error', 8)">error</div>
     <div class="btns warning" @click="toast('متن پیام هشدار', 'warning')">warning</div>
+    <div class="btns dark" @click="toggle_theme">{{ isDark ? 'light-mode' : 'dark-mode' }}</div>
   </div>
 
 </template>
